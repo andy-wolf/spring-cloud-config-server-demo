2c35b86cec9f7f0bfeedd13987f5b045b006999a
Andy Wolf <andy.wolf@sixt.com>
2020-10-09 16:00:09 +0200

    Initial commit

---
+++ src/main/java/de/andywolf/demo/configserver/SpringCloudConfigServerDemoApplication.java
@@ -0,0 +1,13 @@
+package de.andywolf.demo.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/andywolf/demo/configserver/SpringCloudConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.andywolf.demo.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
